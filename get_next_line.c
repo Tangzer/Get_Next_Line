402e1e6c0434f7ad4341b7074dddca4219b4a94c
tverdood <tverdood@student.s19.be>
2022-01-27 16:15:57 +0100

    a

--- get_next_line.c
+++ get_next_line.c
@@ -1,18 +1,49 @@
 #include "get_next_line.h"
 
-static char *ft_get_next_line(int fd, char *line, )
+static char *next_line(char read_line, int i)
 {
+	int j;
 
+	i++;
+	j = 0;
+	while (read_line[i] != '\n')
+		j++;
+	read_line = malloc(sizeof(char) * j + 1);
+	if (!read_line)
+		return (0);
+	return (read_line);
+}
+
+static char *ft_get_next_line(int fd, char *line, char *read_line)
+{
+	int i;
+
+	i = 0;
+	while (read_line[i] != '\n')
+		i++;
+	line = malloc(sizeof(char) * i + 1);
+	if (!line)
+		return (0);
+	i = 0;
+	while (read_line[i] != '\n')
+	{
+		line[i] = read_line[i];
+		i++;
+	}
+	read_line = next_line(read_line, i);
+	return (line);
 }
 
 char	*get_next_line(int fd)
 {
-	char	*line;
-	int		nbytes;
+	char			*line;
+	static char		*read_line;
 
 	line = NULL;
-	nbytes = 0;
-	if (fd < 0 || BUFFER_SIZE < 1)
+	read_line = read(fd, read_line, BUFFER_SIZE);
+	if (!read_line)
+		return (0);
+	if (fd < 0 || fd > 1024 || !fd || BUFFER_SIZE <= 0)
 		return (ft_error(0));
-	return (ft_get_next_line(fd, buff, nbytes));
+	return (ft_get_next_line(fd, line, read_line));
 }
